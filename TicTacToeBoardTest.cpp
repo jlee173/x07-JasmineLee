4d5929f3c1b496ae14797fa661abccf9c10231ac
student <student@softeng>
2017-02-27 12:49:21 -0800

    Jasmine added TTTB.cpp and TTTBtest.cpp

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -14,12 +14,25 @@ class TicTacToeBoardTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor) 
 };
 
-TEST(TicTacToeBoardTest, turnIsNotX)
+TEST(TicTacToeBoardTest, checkFirstSquare)
 {
 	TicTacToeBoard tttb;
 	tttb.clearBoard();
-	ASSERT_FALSE(tttb.placePiece(0,0) == X);		
+	ASSERT_FALSE(tttb.placePiece(0,0) != Blank);		
 }
+
+TEST(TicTacToeBoardTest, checkClear)
+{
+	TicTacToeBoard tttb;
+	tttb.clearBoard();
+	for(int row = 0; row < 3; row++)
+	{
+		for(int col = 0; col < 3; col++)
+		{
+			ASSERT_FALSE(tttb.placePiece(row, col) != Blank);	
+		}
+	}
+}		
 
 
 TEST(TicTacToeBoardTest, sanityCheck)
